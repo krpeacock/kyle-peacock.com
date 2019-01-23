9312aa3cf3aadb699f9347dd75ee052cc9c52506
krpeacock <kylpeacock@gmail.com>
2019-01-22 20:58:51 -0800

    updating about page

--- src/pages/about.js
+++ src/pages/about.js
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:5140572631ca069ac18a9302d3b9b2e09c55019b96dd9425a4120f99d80005bf
-size 1454
+oid sha256:4495ca396b3fba7e39ba08d69a1479b0f5f62c69d2b66723ab17c7bde254991d
+size 1621
