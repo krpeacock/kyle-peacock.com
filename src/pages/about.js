ce1cb56aa96c5522ec8f7e61c5bd6c1b93e97e9f
krpeacock <kylpeacock@gmail.com>
2019-01-22 20:58:51 -0800

    updating about page

--- src/pages/about.js
+++ src/pages/about.js
@@ -16,12 +16,7 @@ const SecondPage = () => (
       <h1>About me</h1>
       <p>
         I am the lead frontend developer at{' '}
-        <a href="https://cuyana.com">Cuyana</a>, the external communications
-        director of the{' '}
-        <a href="https://www.sfendc.com/">
-          San Francisco Eastern Neighborhoods Club
-        </a>
-        , and a singer for the Episcopal Church of St Mary the Virgin.
+        <a href="https://cuyana.com">Cuyana</a> and a singer for the <a href="https://smvsf.org/">Episcopal Church of St Mary the Virgin.</a>
       </p>
       <p>
         I grew up in Walnut Creek and went to college in Illinois where I
@@ -30,12 +25,12 @@ const SecondPage = () => (
       </p>
       <p>
         After spending a semester studying Law at UC Davis, I dropped out to
-        spend six months learning to code at Galvanize, a programming bootcamp.
+        spend six months learning to code at <a href="https://www.galvanize.com/">Galvanize</a>, a programming bootcamp.
       </p>
       <p>
-        Since then, I worked at two marketing agencies, Organic and AKQA, where
-        I built pages for Wells Fargo and Audi USA. I spent a brief contract at
-        Splunk before settling into my current role.
+        Since then, I worked at two marketing agencies, <a href="https://www.organic.com/home">Organic</a> and <a href="https://www.akqa.com/">AKQA</a>, where
+        I built pages for <a href="https://welcome.wf.com/get-college-ready-tracker">Wells Fargo</a> and <a href="https://www.audiusa.com/myaudi">Audi USA</a>, where I re-skinned the My Audi experience. I spent a brief contract at
+        <a href="https://www.splunk.com/">Splunk</a> before settling into my current role.
       </p>
       <Link to="/">Go back to the homepage</Link>
     </Container>
