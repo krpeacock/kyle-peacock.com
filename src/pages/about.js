7cfb7900e3118d1ffd6b48c9eddaa171a9c36cdd
krpeacock <kylpeacock@gmail.com>
2019-03-18 08:46:34 -0700

    fixing issues with gatsby build and styled components

--- src/pages/about.js
+++ src/pages/about.js
@@ -16,7 +16,8 @@ const SecondPage = () => (
       <h1>About me</h1>
       <p>
         I am the lead frontend developer at{' '}
-        <a href="https://cuyana.com">Cuyana</a> and a singer for the <a href="https://smvsf.org/">Episcopal Church of St Mary the Virgin.</a>
+        <a href="https://cuyana.com">Cuyana</a> and a singer for the{' '}
+        <a href="https://smvsf.org/">Episcopal Church of St Mary the Virgin.</a>
       </p>
       <p>
         I grew up in Walnut Creek and went to college in Illinois where I
@@ -25,12 +26,21 @@ const SecondPage = () => (
       </p>
       <p>
         After spending a semester studying Law at UC Davis, I dropped out to
-        spend six months learning to code at <a href="https://www.galvanize.com/">Galvanize</a>, a programming bootcamp.
+        spend six months learning to code at{' '}
+        <a href="https://www.galvanize.com/">Galvanize</a>, a programming
+        bootcamp.
       </p>
       <p>
-        Since then, I worked at two marketing agencies, <a href="https://www.organic.com/home">Organic</a> and <a href="https://www.akqa.com/">AKQA</a>, where
-        I built pages for <a href="https://welcome.wf.com/get-college-ready-tracker">Wells Fargo</a> and <a href="https://www.audiusa.com/myaudi">Audi USA</a>, where I re-skinned the My Audi experience. I spent a brief contract at
-        <a href="https://www.splunk.com/">Splunk</a> before settling into my current role.
+        Since then, I worked at two marketing agencies,{' '}
+        <a href="https://www.organic.com/home">Organic</a> and{' '}
+        <a href="https://www.akqa.com/">AKQA</a>, where I built pages for{' '}
+        <a href="https://welcome.wf.com/get-college-ready-tracker">
+          Wells Fargo
+        </a>{' '}
+        and <a href="https://www.audiusa.com/myaudi">Audi USA</a>, where I
+        re-skinned the My Audi experience. I spent a brief contract at{' '}
+        <a href="https://www.splunk.com/">Splunk</a> before settling into my
+        current role.
       </p>
       <Link to="/">Go back to the homepage</Link>
     </Container>
