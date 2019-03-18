e99c9c904c76a2d4a673cef91578d52b71c1f9db
krpeacock <kylpeacock@gmail.com>
2019-03-18 08:46:34 -0700

    fixing issues with gatsby build and styled components

--- src/pages/about.js
+++ src/pages/about.js
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:4495ca396b3fba7e39ba08d69a1479b0f5f62c69d2b66723ab17c7bde254991d
-size 1621
+oid sha256:80c1e8bda7cfa9b85390de5e85eb00617537709eda85edcb4b570bcf561f2cc6
+size 1740
