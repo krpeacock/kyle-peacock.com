346f7d19de6d09afe1b5b8f8affc4925bca96e83
Kyle Peacock <kyle.peacock@cuyana.com>
2019-03-21 17:01:25 -0700

    fixing localstorage

--- src/components/layout.js
+++ src/components/layout.js
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:57fdb5b159c261c39ee8f5f165d02773852411ec2d06f513773ce9311a06f0ea
-size 2575
+oid sha256:d894ba07fc9c27e375df3317f9d10c9bb29d4064b212ce2b03123f8d10faf6a6
+size 2587
