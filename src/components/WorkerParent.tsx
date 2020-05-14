f677c441badfb0eb21c6064f938bf93b39300766
krpeacock <kylpeacock@gmail.com>
2020-05-14 08:59:38 -0700

    dynamic import for worker SSR

--- src/components/WorkerParent.tsx
+++ src/components/WorkerParent.tsx
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:6b64e1bee1b4ab0c3a001c514aa4c53d1247e82bca4b9dee7257ae5ca2239d6c
-size 737
+oid sha256:b53de019f3ac39264e2a8c9971d509f5aa62390e21d8713f3d22f1e036cb6cab
+size 1083
---
+++ src/components/countListener.worker.ts
@@ -0,0 +1,3 @@
+version https://git-lfs.github.com/spec/v1
+oid sha256:3e9f222dbac77bcc256eeb50ff5d388dac8fc54afad3dbb4c04d4bd006560b34
+size 277
