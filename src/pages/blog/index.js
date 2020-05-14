ad1b74ca2579215fddc7babde4ce35b38abd06d8
krpeacock <kylpeacock@gmail.com>
2020-05-13 22:50:41 -0700

    adding post to index

--- src/pages/blog/index.js
+++ src/pages/blog/index.js
@@ -2,6 +2,7 @@ import React from "react";
 import styled from "styled-components";
 import PowerStrip from "./power-strip";
 import SiteUpgrade from "./site-upgrade";
+import WebWorkers from "./web-workers";
 
 const Banner = styled.div`
   width: 100%;
@@ -12,6 +13,7 @@ const Banner = styled.div`
 const Blog = () => (
   <div>
     <Banner />
+    <WebWorkers />
     <SiteUpgrade />
     <PowerStrip />
   </div>
