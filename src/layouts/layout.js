8b1c2820d6191ae75c16aa03dd0fafea40df19f7
Kyle Peacock <kyle.peacock@coinbase.com>
2020-05-14 12:17:06 -0700

    adding sentry error logging

--- src/layouts/layout.js
+++ src/layouts/layout.js
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:c9953d9248dcb90b0330af3aaaa726c190c11698f042b6567b50288835a91c28
-size 2757
+oid sha256:191fe698dbadcf15f5aac79210ff3362eb32380e52f5034bb74a0d8d8a561b3b
+size 2850
