18ee57ae28133a4e02555e67616b039eb566bba6
krpeacock <kylpeacock@gmail.com>
2020-05-16 00:00:16 -0700

    in case seo tags aren't present

--- src/components/seo.js
+++ src/components/seo.js
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:e418da89f934eabbe0091fd162ace724c8601b5656004b3e23cf0352049620c3
-size 2532
+oid sha256:61eef688bcf85da8ae21032bdbb90af9989d0163f449d1883e3384e10db66b10
+size 2534
