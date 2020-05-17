5a5411785d44b4edaf89619d0ba699ad0b111129
krpeacock <kylpeacock@gmail.com>
2020-05-16 19:30:40 -0700

    jfk blog post

--- src/components/BlogComponents.js
+++ src/components/BlogComponents.js
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:42b10e2fee4f668070ddd9db9db5618b8e42de6ebb40aef40b783bffc9160a87
-size 1340
+oid sha256:11bc3074201258dacae95925cfee67fc94333340571abbd975b17a8c27432a98
+size 1361
