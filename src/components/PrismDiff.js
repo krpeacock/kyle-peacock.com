e7343293f31b6b341b1dc836d0f7dc63a2ccdd49
Kyle Peacock <kyle.peacock@coinbase.com>
2020-05-23 13:41:26 -0700

    cleaning up text shadow

--- src/components/PrismDiff.js
+++ src/components/PrismDiff.js
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:c8bab90742d6b5dc94b5777ff3c0ea1841eeea77ca7bf40a6ceaf90a50017db8
-size 1261
+oid sha256:b2b37d81a7991244504eeec5634b76b2b3e471a970fbc5f8f034706c97e9159f
+size 1309
