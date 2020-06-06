67ddebfe018eb60b93fc8f401481bb95c8e39935
krpeacock <kylpeacock@gmail.com>
2020-06-06 15:18:25 -0700

    custom image for opengraph

--- gatsby-config.js
+++ gatsby-config.js
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:349b58845346b505cfc4a7ab0861c0d3032735fbbc3b9ac61d33f87e7da78976
-size 1582
+oid sha256:45f29a18f092ccb49209c79550dcac08d279bcdac126fc2733f2c0450ddcce6c
+size 1615
