08a9f4843f2b2967042cdf191f961f3cdd306d67
Kyle Peacock <kylpeacock@gmail.com>
2021-08-10 19:48:56 -0700

    nicer visit graphs

---
+++ src/components/PieChart.tsx
@@ -0,0 +1,58 @@
+import React, { useState, ComponentProps } from "react";
+import { PieChart } from "react-minimal-pie-chart";
+
+type Props = {
+  data: ComponentProps<typeof PieChart>["data"];
+};
+
+function FullOption(props: Props) {
+  const [selected, setSelected] = useState<number | undefined>(0);
+  const [hovered, setHovered] = useState<number | undefined>(undefined);
+
+  const data = props.data.map((entry, i) => {
+    if (hovered === i) {
+      return {
+        ...entry,
+        color: "grey",
+      };
+    }
+    return entry;
+  });
+
+  const lineWidth = 60;
+
+  return (
+    <PieChart
+      style={{
+        fontFamily:
+          '"Nunito Sans", -apple-system, Helvetica, Arial, sans-serif',
+        fontSize: "8px",
+      }}
+      className="pie"
+      data={data}
+      radius={PieChart.defaultProps.radius - 6}
+      lineWidth={60}
+      segmentsStyle={{ transition: "stroke .3s", cursor: "pointer" }}
+      segmentsShift={(index) => (index === selected ? 6 : 1)}
+      animate
+      label={({ dataEntry }) => Math.round(dataEntry.percentage) + "%"}
+      labelPosition={100 - lineWidth / 2}
+      labelStyle={{
+        fill: "#fff",
+        opacity: 0.75,
+        pointerEvents: "none",
+      }}
+      onClick={(_, index) => {
+        setSelected(index === selected ? undefined : index);
+      }}
+      onMouseOver={(_, index) => {
+        setHovered(index);
+      }}
+      onMouseOut={() => {
+        setHovered(undefined);
+      }}
+    />
+  );
+}
+
+export default FullOption;
--- src/pages/visits.tsx
+++ src/pages/visits.tsx
@@ -1,25 +1,78 @@
 import * as React from "react";
+import PieChart from "../components/PieChart";
 import Layout from "../layouts/layout";
 import { page_visits } from "../declarations/page_visits";
 import {
   Result_1,
   VisitSummary,
 } from "../declarations/page_visits/page_visits.did";
+import styled from "styled-components";
+import { PropsWithDefaults } from "react-minimal-pie-chart/types/Chart/Chart";
+import { LabelRenderProps } from "react-minimal-pie-chart/types/Label";
+
+const Article = styled.article`
+  margin-bottom: 1rem;
+  width: 100%;
+  border: 1px solid var(--text-color);
+  padding: 8px;
+  @media (min-width: 1080px) {
+    max-width: 32%;
+  }
+  dl {
+    display: grid;
+    grid-template-columns: auto 1fr;
+    dt {
+      margin-right: 8px;
+    }
+  }
+  h4 {
+    margin: 1rem 0 0.5rem;
+    height: 3rem;
+    font-size: 1rem;
+  }
+  .pie {
+    max-width: 30vw;
+    max-height: 30vh;
+  }
+
+  display: grid;
+  grid-template-rows: auto auto 1fr;
+`;
+
+const Label = (props: LabelRenderProps) => {
+  return <p>{props.dataEntry}</p>;
+};
 
 const Summary = (props: { summary: VisitSummary }) => {
   const { summary } = props;
   return (
-    <article key={summary.route}>
-      <h3>{summary.route}</h3>
+    <Article>
+      <h4>
+        <a href={summary.route}>{summary.route}</a>
+      </h4>
       <dl>
         <dt>Total visits:</dt>
         <dd>{summary.total}</dd>
-        <dt>Mobile:</dt>
+        <dt>Mobile visits:</dt>
         <dd>{summary.mobile}</dd>
-        <dt>Desktop:</dt>
+        <dt>Desktop visits:</dt>
         <dd>{summary.desktop}</dd>
       </dl>
-    </article>
+      <PieChart
+        data={[
+          {
+            title: "Mobile",
+            value: summary.mobile,
+            color: "var(--peacock-green)",
+          },
+          {
+            title: "Desktop",
+            value: summary.desktop,
+            color: "var(--red-violet)",
+          },
+        ]}
+      />
+    </Article>
   );
 };
 
@@ -32,7 +85,12 @@ async function fetchSummaries(): Promise<VisitSummary[]> {
       summaries.push(summary.ok);
     }
   }
-  return summaries;
+  if (window.location.host.indexOf("localhost") >= 0) {
+    return summaries;
+  }
+  return summaries.filter((summary) =>
+    window.location.host.includes(summary.route)
+  );
 }
 
 function Visits() {
@@ -46,9 +104,18 @@ function Visits() {
   return (
     <Layout pageContext={null} location={null} data={null}>
       <h1>Page Visits</h1>
-      <section>
+      <section
+        style={{
+          display: "flex",
+          flexFlow: "row wrap",
+          gap: "1rem",
+          justifyContent: "space-evenly",
+        }}
+      >
         {summaries
-          ? summaries.map((summary) => <Summary summary={summary} />)
+          ? summaries.map((summary) => (
+              <Summary summary={summary} key={summary.route} />
+            ))
           : null}
       </section>
     </Layout>
